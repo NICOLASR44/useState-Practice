528ded80f810f3a1b4dd1aef729eb0b2e06a772d
NICOLASR44 <158582207+NICOLASR44@users.noreply.github.com>
2024-07-08 15:08:31 +0200

    exercice 3

--- src/components/Exercice3.jsx
+++ src/components/Exercice3.jsx
@@ -1,22 +1,25 @@
 import Exercice from "./container/Exercice";
-
+import { useState } from "react";
 export default function Exercice3() {
   // 1. Crée un state pour suivre l'état de visibilité du texte.
+  const [visible, setVisible] = useState(false);
   // 2. Implémente une fonction pour basculer a l'état contraire ! .
+  const oppositeVisible = () => {
+    setVisible(!visible);
+  };
   // 3. Passe la fonction de toggle au bouton.
   // 4. Affiche ou masque le texte en fonction de l'état de visibilité.
   return (
     <Exercice>
       <h2>Exercice 3 : Toggle</h2>
       <p className="instructions">
-        Modifie le composant Exercice3 pour basculer entre "Afficher" et
-        "Masquer" un texte.
+        Modifie le composant Exercice3 pour basculer entre "Afficher" et "Masquer" un texte.
       </p>
       <div className="solution">
         <div>
-          <button>Afficher/Masquer</button>
+          <button onClick={oppositeVisible}>Afficher/Masquer</button>
           {/* 5. Affiche conditionnellement le paragraphe en fonction de l'état */}
-          <p>Texte à afficher/masquer</p>
+          {visible && <p>Texte à afficher/masquer</p>}
         </div>
       </div>
     </Exercice>
