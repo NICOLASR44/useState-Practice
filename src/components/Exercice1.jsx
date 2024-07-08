bab6e9c12f92020c5b667f77c880fc8922229618
NICOLASR44 <158582207+NICOLASR44@users.noreply.github.com>
2024-07-08 14:54:01 +0200

    exercice2

--- src/components/Exercice1.jsx
+++ src/components/Exercice1.jsx
@@ -1,22 +1,29 @@
 import Exercice from "./container/Exercice";
-
+import { useState } from "react";
 export default function Exercice1() {
   // 1. Crée un state qui garde en mémoire la valeur du compteur.
   // 2. Implémente une fonction pour incrémenter la valeur du compteur.
   // 3. Implémente une fonction pour décrémenter la valeur du compteur.
   // 4. Passe les fonctions d'incrémentation et de décrémentation aux boutons correspondants.
+  const [count, setCount] = useState(0);
+  const updateCount = () => {
+    setCount(count + 1);
+  };
+  const inverseCount = () => {
+    setCount(count - 1);
+  };
+
   return (
     <Exercice>
       <h2>Exercice 1 : Compteur simple</h2>
       <p className="instructions">
-        Modifie le composant Exercice1 pour faire fonctionner le compteur et ses
-        deux boutons.
+        Modifie le composant Exercice1 pour faire fonctionner le compteur et ses deux boutons.
       </p>
       <div className="solution">
         <div>
-          <button>-</button>
-          <button>+</button>
-          <p>Compteur : 0</p>
+          <button onClick={inverseCount}>-</button>
+          <button onClick={updateCount}>+</button>
+          <p>Compteur : {count}</p>
         </div>
       </div>
     </Exercice>
--- src/components/Exercice2.jsx
+++ src/components/Exercice2.jsx
@@ -1,21 +1,25 @@
 import Exercice from "./container/Exercice";
-
+import { useState } from "react";
 export default function Exercice2() {
   // 1. Crée un state qui garde en mémoire la valeur de l'input.
+  const [inputTab, setInputTab] = useState("");
   // 2. Implémente une fonction pour mettre à jour la valeur du state lorsque l'utilisateur tape dans l'input.
+  const handleImputAdd = (addMaj) => {
+    setInputTab(addMaj.target.value);
+  };
   // 3. Passe la fonction de mise à jour au champ de texte pour qu'il soit contrôlé.
   // 4. Affiche la valeur du state sous le champ de texte.
+
   return (
     <Exercice>
       <h2>Exercice 2 : Input contrôlé</h2>
       <p className="instructions">
-        Modifie le composant Exercice2 pour affichez la valeur du champ de texte
-        sous celui-ci.
+        Modifie le composant Exercice2 pour affichez la valeur du champ de texte sous celui-ci.
       </p>
       <div className="solution">
         <div>
-          <input type="text" />
-          <p>"Valeur de l'input"</p>
+          <input type="text" value={inputTab} onChange={handleImputAdd} />
+          <p>{inputTab}</p>
         </div>
       </div>
     </Exercice>
